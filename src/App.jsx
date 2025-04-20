0d39bdbc8f0c5f67af0573e93135f9a64ee90a75
jefersonlimacj <jeferson.lima.cj@gmail.com>
2025-04-20 14:35:35 -0300

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Agora eu já fiz o deploy
         </p>
       </div>
       <p className="read-the-docs">
